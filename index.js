c9b3ce97ebc2c36216f7fdb55e457bd93a0af3ae
Nayimh <hnaim6979@gmail.com>
2021-10-29 01:20:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config()
+
+const app = express();
+
+const ObjectedId = require('mongodb').ObjectId;
+const { MongoClient } = require('mongodb');
+
+
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cetyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('TravellData');
+        const servicesCollection = database.collection('services');
+        const doc = {
+            name: "nayim",
+            email: "hnaim51@gmail.com"
+        }
+        const result = await servicesCollection.insertOne(doc);
+        
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('my server ready to use.')
+})
+
+app.listen(port, () => {
+    console.log('listning to port', port)
+})
